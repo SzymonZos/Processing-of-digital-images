ec325dd8c8f791a9332bf14b9188acb20ddf3736
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2019-12-18 16:18:11 +0100

    piw done

--- PIW/example.py
+++ PIW/example.py
@@ -1,45 +1,31 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import os
 
+inputPath = os.path.dirname(os.path.abspath(__file__)) + r'\images'
+outputPath = os.path.dirname(os.path.abspath(__file__)) + r'\output-dir'
+originalImages = {file: cv2.imread(r'{0}\{1}'.format(inputPath, file)) for file in os.listdir(inputPath)}
+initClusters = (cv2.KMEANS_RANDOM_CENTERS, cv2.KMEANS_PP_CENTERS)
+titles = ['Quantised image using k-means algorithm, where K = %i',
+          'Quantised image using k-means++ algorithm, where K = %i']
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-original_image = cv2.imread('baboon.bmp')
-
-img=cv2.cvtColor(original_image,cv2.COLOR_BGR2RGB)
-vectorized = img.reshape((-1,3))
-vectorized = np.float32(vectorized)
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-
-
-K = 10
-attempts=100
-ret,label,center=cv2.kmeans(vectorized,K,None,criteria,attempts,cv2.KMEANS_RANDOM_CENTERS)
-
-
-center = np.uint8(center)
-
-res = center[label.flatten()]
-result_image = res.reshape((img.shape))
-
-figure_size = 15
-plt.figure(figsize=(figure_size,figure_size))
-plt.subplot(1,2,1),plt.imshow(img)
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(1,2,2)
-
-plt.imshow(result_image)
-plt.title('Segmented++ Image when K = %i' % K), plt.xticks([]), plt.yticks([])
-plt.show()
+for filename, img in originalImages.items():
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    vectorized = np.float32(img.reshape((-1, 3)))
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    for quantizationLevels in range(1, 8):
+        attempts = 10
+        quantisedImages = list()
+        for cluster in initClusters:
+            [ret, label, center] = cv2.kmeans(vectorized, 2 ** quantizationLevels, None, criteria, attempts, cluster)
+            quantisedImages.append(np.uint8(center)[label.flatten()].reshape(img.shape))
+        plt.figure(figsize=(20, 8))
+        plt.subplot(1, 3, 1), plt.imshow(img)
+        plt.title('Original image'), plt.xticks([]), plt.yticks([])
+        for index, (quantisedImg, title) in enumerate(zip(quantisedImages, titles)):
+            plt.subplot(1, 3, 2 + index), plt.imshow(quantisedImg)
+            plt.title(title % 2 ** quantizationLevels + '\nPSNR = ' + str(cv2.PSNR(img, quantisedImg)))
+            plt.xticks([]), plt.yticks([])
+        plt.savefig(r'{0}\{1}_{2}'.format(outputPath, 2 ** quantizationLevels, filename), format='png')
+        plt.show()
