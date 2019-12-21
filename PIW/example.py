c2aca4d8d0d5dc1e6ef2709ce783c38e307326ab
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2019-12-21 13:39:59 +0100

    Refactored code, it is more readable now

--- PIW/example.py
+++ PIW/example.py
@@ -2,29 +2,57 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import os
+import time
+from collections import defaultdict
 
-inputPath = os.path.dirname(os.path.abspath(__file__)) + r'\images'
-outputPath = os.path.dirname(os.path.abspath(__file__)) + r'\output-dir'
+projectPath = os.path.dirname(os.path.abspath(__file__))
+logFile = open(projectPath + r'\logs.txt', 'w')
+inputPath = projectPath + r'\images'
+outputPath = projectPath + r'\output-dir'
 originalImages = {file: cv2.imread(r'{0}\{1}'.format(inputPath, file)) for file in os.listdir(inputPath)}
-initClusters = (cv2.KMEANS_RANDOM_CENTERS, cv2.KMEANS_PP_CENTERS)
+algorithms = {'kmeans': cv2.KMEANS_RANDOM_CENTERS, 'kmeans++': cv2.KMEANS_PP_CENTERS}
 titles = ['Quantised image using k-means algorithm, where K = %i',
           'Quantised image using k-means++ algorithm, where K = %i']
-# łącznie 10 obrazów; 10 próbek dla każdego algorytmu
-for filename, img in originalImages.items():
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+def perform_algorithms(img, quantization_levels):
+    quantization_data = defaultdict(dict)
     vectorized = np.float32(img.reshape((-1, 3)))
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    for quantizationLevels in range(1, 9): # poziomy od 8 od 256
-        attempts = 10
-        quantisedImages = list()
-        for cluster in initClusters: # tu zmierzyć czas
-            [ret, label, center] = cv2.kmeans(vectorized, 2 ** quantizationLevels, None, criteria, attempts, cluster)
-            quantisedImages.append(np.uint8(center)[label.flatten()].reshape(img.shape))
-        plt.figure(figsize=(20, 8))
-        plt.subplot(1, 3, 1), plt.imshow(img)
-        plt.title('Original image'), plt.xticks([]), plt.yticks([])
-        for index, (quantisedImg, title) in enumerate(zip(quantisedImages, titles)):
-            plt.subplot(1, 3, 2 + index), plt.imshow(quantisedImg)
-            plt.title(title % 2 ** quantizationLevels + '\nPSNR = ' + str(cv2.PSNR(img, quantisedImg)))
-            plt.xticks([]), plt.yticks([])
-        plt.savefig(r'{0}\{1}_{2}'.format(outputPath, 2 ** quantizationLevels, filename), format='png', bbox_inches='tight')
+    for (algorithm_name, algorithm_value), title in zip(algorithms.items(), titles):  # tu zmierzyć czas
+        start_timer = time.time()
+        _, label, center = cv2.kmeans(vectorized, 2 ** quantization_levels, None, criteria, attempts=10,
+                                      flags=algorithm_value)
+        end_timer = time.time()
+        quantization_data[algorithm_name]['image'] = np.uint8(center)[label.flatten()].reshape(img.shape)
+        quantization_data[algorithm_name]['elapsed_time'] = end_timer - start_timer
+        quantization_data[algorithm_name]['title'] = title
+    return quantization_data
+
+
+def save_results(filename, img, quantization_levels, quantization_data):
+    plt.figure(figsize=(20, 8))
+    plt.subplot(1, 3, 1), plt.imshow(img)
+    plt.title('Original image'), plt.xticks([]), plt.yticks([])
+    for index, algorithm_name in enumerate(quantization_data):
+        plt.subplot(1, 3, 2 + index), plt.imshow(quantization_data[algorithm_name]['image'])
+        title = quantization_data[algorithm_name]['title'] % 2 ** quantization_levels + \
+                '\nPSNR = ' + str(cv2.PSNR(img, quantization_data[algorithm_name]['image']))
+        logFile.write(title + '\n')
+        plt.title(title), plt.xticks([]), plt.yticks([])
+    output_filename = r'\{0}_{1}'.format(2 ** quantization_levels, filename)
+    logFile.write(output_filename + '\n\n\n')
+    plt.savefig(outputPath + output_filename, format='png', bbox_inches='tight')
+
+
+# łącznie 10 obrazów; 10 próbek dla każdego algorytmu
+def main():
+    for filename, img in originalImages.items():
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        for quantization_levels in range(1, 2):  # poziomy od 8 od 256
+            quantization_data = perform_algorithms(img, quantization_levels)
+            save_results(filename, img, quantization_levels, quantization_data)
+
+
+if __name__ == '__main__':
+    main()
